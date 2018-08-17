2d6e74deaee9ad5940ee7c78d5f031ea5dd96ccb
jtb1137 <jtb1137@gmail.com>
2018-08-17 15:30:55 -0400

    build index and detail templates, build index and detail views

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,21 @@
+#from django.template import loader
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+from .models import Post
+
+
 def index(request):
-  #return HttpResponse('Hello, world')
-  return render(request, 'posts/index.html')
+  latest_posts = Post.objects.order_by('-pub_date')[:5]
+  #template = loader.get_template('posts/index.html')
+  context = {'latest_posts': latest_posts}
+  # return HttpResponse(template.render(context, request))
+  return render(request, 'posts/index.html', context)
+
+
+def detail(request, post_id):
+  try:
+    post = Post.objects.get(post_id)
+  except Post.DoesNotExist:
+    raise Http404("Post does not exist")
+  return render(request, 'posts/detail.html', {'post': post})
