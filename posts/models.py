699ba334afcd8850a28bc87e76011481b587e779
jtb1137 <jtb1137@gmail.com>
2018-08-17 14:28:52 -0400

    continue to build out Post model, register Post with admin

--- posts/models.py
+++ posts/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from datetime import datetime, timezone, timedelta
 
 # Create your models here.
-class Posts(models.Model):
+class Post(models.Model):
   title = models.CharField(max_length=100)
   body = models.TextField()
   pub_date = models.DateTimeField('date published')
+
+  def __str__(self):
+    return self.title
+  
+  def was_published_recently(self):
+    return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
