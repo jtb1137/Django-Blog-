c924b2b8ed1f5f8e4ceec76421ba302c571a2693
jtb1137 <jtb1137@gmail.com>
2018-08-17 14:09:48 -0400

    posts model, begin defining posts routes

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Posts(models.Model):
+  title = models.CharField(max_length=100)
+  body = models.TextField()
+  pub_date = models.DateTimeField('date published')
